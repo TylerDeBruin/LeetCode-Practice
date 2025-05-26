e9bbffa251c7956217a6f52c8d61144feb0acad7
Tyler DeBruin <tdebruin@1800contacts.com>
2025-05-26 15:12:56 -0600

    MajorityElementSolution

---
+++ NeetCode/Arrays/MajorityElementSolution.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeetCode.Arrays
+{
+    public class MajorityElementSolution
+    {
+        public int MajorityElement(int[] nums)
+        {
+            var lookup = new Dictionary<int, int>();
+
+            foreach (var element in nums)
+            {
+                if (lookup.ContainsKey(element))
+                {
+                    lookup[element]++;
+                }
+                else
+                {
+                    lookup.Add(element, 1);
+                }
+            }
+
+            return lookup.OrderByDescending(x => x.Value).First().Key;
+        }
+    }
+} 
